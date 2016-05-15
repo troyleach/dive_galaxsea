adffb16ec90fdc8f0ef8d921c2648b0f08b47579
Troy Leach <troyleach29@gmail.com>
2016-05-14 21:12:25 -0500

    moved the action mailer to just after the new user is saved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      AdminMailer.admin_email(@user).deliver_later
       render json: @user, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
