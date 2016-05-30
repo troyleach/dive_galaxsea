a3c1bc0ee1170098d4f9593ca55973ddd1398523
Troy Leach <troyleach29@gmail.com>
2016-05-30 12:02:58 -0500

    in dev mode, took kims email off

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,7 +4,8 @@ class AdminMailer < ApplicationMailer
   def admin_email(user)
     @user = user
     @url = ENV["DIVEGALAXSEA_HOST"]
-    @recipients = ['troyleach@outlook.com', 'kim@divegalaxsea.com']
+    @recipients = ['troyleach@outlook.com']
+    #@recipients = ['troyleach@outlook.com', 'kim@divegalaxsea.com']
 
     mail(to: @recipients,
          subject: 'YOU HAVE A NEW CLIENT'
