9a849a1d09f54314e4d0cbf63a4cb4bfc451a770
Troy Leach <troyleach29@gmail.com>
2017-01-22 18:14:01 -0600

    added a error clause

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,13 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate
   def create
     user = User.find_by(email: auth_params[:email])
-    if user.authenticate(auth_params[:password])
+    if user && user.authenticate(auth_params[:password])
       jwt = Auth.issue( {user: user.id,
                          admin: user.admin} )
       render json: {access_token: jwt}
     else
+      puts 'this did not work'
+      render json: {error: 'You have been denied'}
     end
   end
 
