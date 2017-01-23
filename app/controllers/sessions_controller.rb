14618b86f4748806c7537c04e0143b8c6e3dbf95
Troy Leach <troyleach29@gmail.com>
2017-01-22 18:54:33 -0600

    yes I have a binding in here

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,15 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate
   def create
     user = User.find_by(email: auth_params[:email])
+    binding.pry
     if user.authenticate(auth_params[:password])
       jwt = Auth.issue( {user: user.id,
                          admin: user.admin} )
       puts "this is the jwt #{jwt}"
-      render json: {access_token: jwt}
+      render json: {access_token: jwt, status: 200}
     else
-      puts 'this did not work'
-      render json: {error: 'You have been denied'}
+      puts 'this did not work fucker face'
+      render json: {error: 'You have been denied', status: 444}
     end
   end
 
