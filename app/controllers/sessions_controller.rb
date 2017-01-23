811041c5edfee3b7062f85cc5ca2f6bcbcb8c7f2
Troy Leach <troyleach29@gmail.com>
2017-01-22 20:46:08 -0600

    fixed the bad response thing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,17 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate
   def create
-    puts ' * ' * 100
-    puts params
     user = User.find_by(email: auth_params[:email])
-    puts ' * passwords' * 100
-    puts user.password
-    puts auth_params[:password]
-    puts user.authenticate(auth_params[:password])
     if user.authenticate(auth_params[:password])
       jwt = Auth.issue( {user: user.id,
                          admin: user.admin} )
-      puts "this is the jwt #{jwt}"
       render json: {access_token: jwt, status: 200}
     else
-      puts 'this did not work fucker face'
       render json: {error: 'You have been denied'}, status: 401
-      # render json: { error: @person.errors }, status: :bad_request
     end
   end
 
   def logout
-    puts " *" * 100
-    puts 'this did some shit'
   end
 
   private
